37acd06f6910ed5017ab8d6693ad2b718d252fa1
Evan <evvn@outlook.com>
2020-05-12 14:08:16 +1000

    github docs folder for pages

---
+++ docs/precache-manifest.2b304fd8ae42314cd8fbd63f6f30b803.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6e5f091dcb1d92e442a44d835fe8bc",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.fcda93780ca437bfdda0.bundle.js"
+  },
+  {
+    "url": "runtime~main.fcda93780ca437bfdda0.bundle.js"
+  },
+  {
+    "revision": "07e46c8e935a536c8968",
+    "url": "static/css/main.86f55aad.chunk.css"
+  },
+  {
+    "revision": "eff41a897be407552d11",
+    "url": "static/css/vendors~main.7b74de8b.chunk.css"
+  },
+  {
+    "revision": "083fecfe931bc481738cd634303faea6",
+    "url": "static/media/chevron-down-12px.083fecfe.svg"
+  },
+  {
+    "revision": "7c7702e88b317e85ed8e1bbf99be2fce",
+    "url": "static/media/chevron-left-12px.7c7702e8.svg"
+  },
+  {
+    "revision": "52a2395c6698a46633dc074e0426348f",
+    "url": "static/media/chevron-right-12px.52a2395c.svg"
+  },
+  {
+    "revision": "487ba3c23bebf0a4287fde7193fcdfb3",
+    "url": "static/media/chevron-up-12px.487ba3c2.svg"
+  },
+  {
+    "revision": "a5927f926e019c96fddd4d5e87bf56f1",
+    "url": "static/media/close-clear-12px.a5927f92.svg"
+  },
+  {
+    "url": "vendors~main.fcda93780ca437bfdda0.bundle.js"
+  },
+  {
+    "url": "vendors~main.fcda93780ca437bfdda0.bundle.js.LICENSE.txt"
+  }
+]);
